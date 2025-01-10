4bf2be6cfe37d8d779763431baa514569e3da091
Mohammad <mhamdl2amen@gmail.com>
2025-01-10 16:23:09 +0200

    plot a new siries

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ sets=pd.read_csv('data/sets.csv')
 #create new siries with the year of the set
 sets_by_years=sets.groupby('year').count()
 sets_by_years['set_num'].head()#give value of set_num of years
-plt.plot(sets_by_years.index,sets_by_years['set_num'])#plot the graph
+plt.plot(sets_by_years.index[:-2],sets_by_years['set_num'][:-2])#plot the graph
 plt.show()#show the graph
+plt.savefig('sets_by_years.png')#save the graph
